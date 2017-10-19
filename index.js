53137888877dd5511ea423547bd7727c505ce81f
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-19 19:08:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+let driverId = 0
+class Driver {
+  constructor (name) {
+    this.id = ++ driverId 
+    this.name = name 
+  }
+
+  trips() {
+    return trips.filter(trip => {
+      return trip.driverId === this.id
+    })
+  }
+}
