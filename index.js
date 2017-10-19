fa40aa9c05cc99bce63935370100501206e03874
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-19 19:08:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 let driverId = 0
 class Driver {
   constructor (name) {
-    this.id = ++ driverId 
-    this.name = name 
+    this.id = ++ driverId
+    this.name = name
   }
 
   trips() {
     return trips.filter(trip => {
       return trip.driverId === this.id
     })
+  }
+
+  passengers() {
+    return passengers.filter()
   }
 }
